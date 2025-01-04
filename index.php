8773464003a297e87e6871177e79374919158ef3
Harsha Pathmasiri <79796988+harshapathmasiri@users.noreply.github.com>
2025-01-04 09:42:03 +0530

    index.php

--- index.php
+++ index.php
@@ -12,7 +12,7 @@ if (isset($_SESSION["clientLogin"])) {
 }
 ?>
 
-<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "http://www.w3.org/TR/xhtml1/DTD/xhtml1-transitional.dtd">
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.0 Transitional//EN" "https://harshapathmasiri.github.io/magicmeal.lk/">
 <html xmlns="http://www.w3.org/1999/xhtml">
     <head>
         <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
